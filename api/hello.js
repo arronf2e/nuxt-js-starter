d63477748fe5fd3da3208984b653e3dac3c0c124
arron <arronf2e@163.com>
2022-10-31 15:53:25 +0800

    first commit

---
+++ api/hello.js
@@ -0,0 +1,5 @@
+export default axios => ({
+  good() {
+    console.log('good')
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-if="$nuxt.isOffline">You are offline</div>
+    <div class="item" v-for="item in dataList" :key="item.id">
+      <p>{{item.title}}</p>
+      <img :src="item.pic" alt="">
+      <div>{{item.desc}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      dataList: [],
+    }
+  },
+  // async mounted() {
+  //   const result = await this.$axios.get('https://api.vvhan.com/api/hotlist?type=huPu')
+  //   this.dataList = result.data.data
+  // },
+  async asyncData({ $axios }) {
+    const result = await $axios.get('https://api.vvhan.com/api/hotlist?type=huPu')
+    console.log(result?.data)
+    return {
+      dataList: result.data.data,
+    }
+  }
+}
+</script>
+<style scoped>
+.item {
+  border-bottom: 1px solid #000;
+}
+</style>
